6e915825ead42eb86fd7e3bb24c26421467f153b
naguilar12 <n.aguilar@uniandes.edu.co>
2018-05-09 21:43:18 -0500

    Mongo test

--- server/main.js
+++ server/main.js
@@ -1,4 +1,11 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from "meteor/mongo";
+
+const Test = new Mongo.Collection("test");
+
+Test.insert({
+  mensage:"This",
+})  
 
 Meteor.startup(() => {
   // code to run on server at startup
